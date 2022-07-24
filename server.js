4cc9a75e9397240a480734a43dda44b830f9cac3
“amiralibhatti” <amiralibhatti143@gmail.com>
2022-07-24 14:54:47 +0500

    My first tiny apollo server project

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const { makeExecutableSchema } = require("@graphql-tools/schema");
+const { loadFilesSync } = require("@graphql-tools/load-files");
+const { ApolloServer } = require("apollo-server-express");
+const typesArray = loadFilesSync(path.join(__dirname, "**/*.graphql"));
+const resolversArray = loadFilesSync(path.join(__dirname, "**/*.resolvers.js"));
+
+async function startApolloServer() {
+  const app = express();
+  const schema = makeExecutableSchema({
+    typeDefs: typesArray,
+    resolvers: resolversArray,
+  });
+
+  const server = new ApolloServer({ schema });
+  await server.start();
+  server.applyMiddleware({ app, path: "/graphql" });
+
+  app.listen(9000, () => {
+    console.log("Running GraphQL server...");
+  });
+}
+
+startApolloServer();
